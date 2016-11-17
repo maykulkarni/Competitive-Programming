bd79b399722faeeb4cdaa1019e45605be7bb370a
Mayur Kulkarni <mayurkulkarni012@gmail.com>
2016-11-17 08:35:29 +0530

    Codeforces updated

--- CS97SI/ChocolateChipCookies.java
+++ CS97SI/ChocolateChipCookies.java
@@ -1,10 +1,27 @@
 package CS97SI;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
- * TODO
+ * Hitlist
  * Created by Mayur Kulkarni on 11/14/2016.
  * Email : mayurkulkarni012@gmail.com
  */
+class Line {
+    double a;
+    double b;
+    double c;
+    // a*x + b*y + c = 0
+
+    public Line(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
+
 class Point {
     double x;
     double y;
@@ -17,17 +34,63 @@ class Point {
     public double distanceTo(Point other) {
         return Math.sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
     }
-
 }
 
 public class ChocolateChipCookies {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        List<Point> list = new ArrayList<>();
+        while (in.hasNext()) {
+            list.add(new Point(in.nextDouble(), in.nextDouble()));
+        }
+        new ChocolateChipCookiesSolver().solve(list);
     }
 }
 
 class ChocolateChipCookiesSolver {
-    public void solve() {
+    public void solve(List<Point> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).distanceTo(list.get(j)) > 5.0)
+                    continue;
+                Line bisector = getBisector(list.get(i), list.get(j));
+            }
+        }
+    }
 
+    private Line getBisector(Point one, Point two) {
+        return null;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

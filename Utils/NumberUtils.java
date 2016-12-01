24066ef6146f61d2fa67f24eedc944b3a26e245d
Mayur Kulkarni <mayurkulkarni012@gmail.com>
2016-12-01 15:24:34 +0530

    Catches updated

--- Utils/NumberUtils.java
+++ Utils/NumberUtils.java
@@ -30,6 +30,25 @@ public class NumberUtils {
         return ans;
     }
 
+    public static long modpow(long a, long b, long mod) {
+        if (b == 1) {
+            return a;
+        }
+        if (b == 0) {
+            return 1;
+        }
+        long ans = 1;
+        while (b > 0) {
+            if ((b & 1) != 0) {
+                ans = (ans * a) % mod;
+            }
+            a = (a * a) % mod;
+            b >>= 1;
+        }
+        return ans % mod;
+
+    }
+
     public static int getMinIntIndex(int[] arr, int endIndex) {
         int maxIndex = 0;
         int maxVal = Integer.MIN_VALUE;

b36ff2eab32908bc07f2488c492552d2a5e7f9f0
Mayur Kulkarni <mayurkulkarni012@gmail.com>
2017-02-06 00:25:38 +0530

    Added docstrings in Blog/Graph
    Signed-off-by: Mayur Kulkarni <mayurkulkarni012@gmail.com>

--- Random/Demo.java
+++ Random/Demo.java
@@ -1,11 +1,17 @@
 package Random;
 
+
+import java.util.Map;
+
 /**
  * Created by mayur on 4/9/16.
  */
 public class Demo {
-    public static void main(String args[]) {
-//        int number = 9;
-        System.out.println((char) 1);
+    public static void ins(Map<Integer, Integer> map, int key) {
+        map.compute(key, (k, v) -> v == null ? 1 : v + 1);
     }
+    public static void main(String args[]) {
+
+    }
+
 }

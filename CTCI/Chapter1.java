bb63a6c288809feed15e9680ceae04cf190741b3
Mayur Kulkarni <mayurkulkarni012@gmail.com>
2018-04-04 14:59:00 +0530

    Started CTCI
    Signed-off-by: mayur3086 <mayurkulkarni012@gmail.com>

---
+++ CTCI/Chapter1.java
@@ -0,0 +1,32 @@
+package CTCI;
+
+import java.util.BitSet;
+import java.util.Scanner;
+
+
+/**
+ * @author Mayur Kulkarni <mayurkulkarni012@gmail.com>
+ */
+public class Chapter1 {
+
+    public static void isUnique(String input) {
+        BitSet mask = new BitSet();
+        boolean isUnique = true;
+        for (char i : input.toCharArray()) {
+            int ascii = (int) i;
+            if (mask.get(ascii)) {
+                System.out.println("Duplicate char: " + i);
+                isUnique = false;
+            } else {
+                mask.set(ascii);
+            }
+        }
+        if (isUnique) System.out.println("Is Unique!");
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String inp = in.next();
+        isUnique(inp);
+    }
+}
